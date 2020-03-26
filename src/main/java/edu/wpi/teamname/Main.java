874fd969e7796dafa3d17bece9b6871465b8a845
Noah-C-S <npcapucillishata@wpi.edu>
2020-03-26 16:03:15 -0400

    added my name (Noah)

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
   public static void main(String[] args) {
     System.out.println("Benjamin Klaiman");
+    System.out.println("Noah Capucilli-Shatan");
     App.launch(App.class, args);
   }
 }
