a96fea2f16008ef4d3512f5ba4ed1311071d1dcb
Robear Mankaryous <rmankaryous12@gmail.com>
2020-03-26 16:14:02 -0400

    added Robear Mankaryous

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -5,6 +5,7 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Benjamin Klaiman");
     System.out.println("Winnie Ly");
+    System.out.println("Robear Mankaryous");
     App.launch(App.class, args);
   }
 }
