f20cb4023e7c2a3d5b9b7f36b3231d2178e7ff6c
bnklaiman <47163915+bnklaiman@users.noreply.github.com>
2020-03-26 16:22:57 -0400

    added a space between my first and last name to test commit

--- src/main/java/edu/wpi/teamname/App.java
+++ src/main/java/edu/wpi/teamname/App.java
@@ -1,4 +1,4 @@
-package edu.wpi.teamname;
+ package edu.wpi.teamname;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -3,7 +3,7 @@ package edu.wpi.teamname;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Benjamin Klaiman");
+    System.out.println("Benjamin  Klaiman");
     System.out.println("Alejandra Garza");
     System.out.println("Noah Capucilli-Shatan");
     System.out.println("Winnie Ly");
